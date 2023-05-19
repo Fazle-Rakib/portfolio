cc3f1f44b7aa5658d4e864b5cc22eac76d157ed2
Fazle Rabbi Rakib <fazle.rabbi@pathao.com>
2023-05-20 00:24:18 +0600

    Update: Package & handled urql exception error

--- src/modules/urql.js
+++ src/modules/urql.js
@@ -1,10 +1,11 @@
-import urql from '@urql/vue'
+import urql, { cacheExchange, fetchExchange } from '@urql/vue'
 
 export const install = ({ isClient, app }) => {
   // if (!isClient) return
 
   app.use(urql, {
     url: 'https://api.github.com/graphql',
+    exchanges: [cacheExchange, fetchExchange],
     fetchOptions: () => {
       const token = import.meta.env.VITE_GITHUB_TOKEN
       return {
