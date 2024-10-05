26de754c36ae517e351c7e06b9e92bd09bdd435f
Fazle-Rakib <fazle30@student.sust.edu>
2024-10-05 07:21:28 +0600

    Refact: Breaking article component hero portion to a child-component

---
+++ src/components/article/ArticlesHero.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hero">
+      <div class="container">
+        <h1 class="hero__headline">Articles</h1>
+        <p class="hero__description">
+           Here you can find my recent writings, blog, and research.
+        </p>
+      </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.hero {
+  background-color: var(--accent-color-lighter, var(--color-gray-800));
+  box-shadow: var(--elevation-2);
+}
+
+.container {
+  padding: calc(2rem + 1vw) 0;
+}
+
+.hero__headline {
+  font-size: var(--text-2xl);
+}
+
+.hero__description {
+  font-size: var(--text-lg);
+  max-width: 800px;
+}
+
+
+body[data-theme='light'] .hero {
+  box-shadow: var(--elevation-3);
+}
+
+@media screen and (max-width: 768px) {
+  .hero .container {
+    padding: 1rem 2.4rem;
+  }
+}
+
+</style>
